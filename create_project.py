3c593bf1ab0dd6ef85acd3a4360609144421fa19
Danila <dmozh58@gmail.com>
2019-10-21 11:38:19 +0300

    adding function for creating aiohttp projects on windows os

---
+++ create_project.py
@@ -0,0 +1,96 @@
+import sys, os, templates
+
+def main():
+    type_project = ''
+    name = ''
+    path = ''
+    d_path = """D:/Projects/PycharmProjects"""
+    # d_path = """C:/Projects/"""
+
+    params = {}
+    key = ''
+    params['-p'] = d_path
+    for i in sys.argv:
+        if (i == '-n') or (i == '-p') or (i == '-t'):
+            key = i
+            continue
+        else:
+            if key != '':
+                if key=='-n':
+                    name = i
+                elif key=='-p':
+                    path = i
+                elif key == '-t':
+                    type_project = i
+    if path == '':
+        path = d_path
+
+    tree_project(path, type_project, name)
+
+def tree_project(path, type, name):
+    slash = '/'
+    #path to dirs
+    root_prj = path+slash+name
+    app = root_prj+slash+'app'
+    views = app+slash+'views'
+    #creates
+    if type != '':
+        if type == 'aiohttp':
+            try:
+                os.mkdir(root_prj)
+
+                file = open(root_prj+slash+'entry.py', 'w')
+                file.write(templates.entry)
+                file.close()
+
+                file = open(root_prj+slash+'credentials.py', 'w')
+                file.write(templates.credentials)
+                file.close()
+
+                file = open(root_prj + slash + 'utils.py', 'w')
+                file.write(templates.utils)
+                file.close()
+            except OSError:
+                print(OSError.errno)
+            finally:
+                print('Project root is created')
+
+            try:
+                os.mkdir(app)
+
+                file = open(app + slash + '__init__.py', 'w')
+                file.write(templates.app_init)
+                file.close()
+
+                file = open(app + slash + 'app.py', 'w')
+                file.write(templates.app)
+                file.close()
+
+                file = open(app + slash + 'routes.py', 'w')
+                file.write(templates.routes)
+                file.close()
+            except OSError:
+                print(OSError.errno)
+            finally:
+                print('App dir is created')
+
+            try:
+                os.mkdir(views)
+
+                file = open(views + slash + '__init__.py', 'w')
+                file.write(templates.views_init)
+                file.close()
+
+                file = open(views + slash + 'response.py', 'w')
+                file.write(templates.response)
+                file.close()
+
+            except OSError:
+                print(OSError.errno)
+            finally:
+                print('Views dir is created')
+        else:
+            print('incorrect key')
+
+if __name__ == "__main__":
+    main()
---
+++ templates.py
@@ -0,0 +1,79 @@
+entry = \
+"""  
+from aiohttp import web
+import argparse
+from app import create_app
+
+app = create_app()
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args.path, args.port)
+    web.run_app(app, path=args.path, port=args.port)
+"""
+
+credentials = \
+"""
+#this file uses for creds to project
+#its need add to git ignore
+"""
+
+utils = \
+"""
+#this file have util function
+"""
+
+app_init = \
+"""
+from .app import create_app
+"""
+
+app = \
+"""
+from aiohttp import web
+
+from .routes import setup_routes
+
+async def create_app():
+    app = web.Application()
+    setup_routes(app)
+    return app
+"""
+
+routes = \
+"""
+from .views import response
+import credentials as crs
+
+def setup_routes(app):
+    #app.router.add_route('GET/POST', f'%url%', response.%func%)
+    pass
+"""
+
+views_init = \
+"""
+from . import response
+"""
+
+response = \
+"""
+from aiohttp import web
+import json
+
+# this file using for response from server
+
+async def test(request):
+    headers = {'Access-Control-Allow-Origin': '*', 'content-type': 'text/html'}
+    response = f'<div>test</div>'
+    return web.Response(body=response, headers=headers)
+    
+async def test_json(request):
+    headers = {'Access-Control-Allow-Origin': '*'}
+    if request.method == 'POST':
+        response_msg = {}
+        
+        response_msg['msg'] = 'test'
+        return web.json_response(response_msg, headers=headers)
+    else:
+        return web.Response(text='get', headers=headers)
+"""
+
