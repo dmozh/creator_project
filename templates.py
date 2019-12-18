6fd6e7172d5c0f51bf799fb8d643bd878be2df56
Danila <dmozh58@gmail.com>
2019-12-18 16:10:24 +0300

    upd templates

--- templates.py
+++ templates.py
@@ -31,14 +31,28 @@ from .app import create_app
 
 app = \
 """
+import base64
+from cryptography import fernet
 from aiohttp import web
-
+from aiohttp_session import setup, get_session, session_middleware
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from .routes import setup_routes
 
+#simple create app
 async def create_app():
     app = web.Application()
     setup_routes(app)
     return app
+    
+#create app for using ws
+#async def create_app():
+#    app = web.Application()
+#    fernet_key = fernet.Fernet.generate_key()
+#    secret_key = base64.urlsafe_b64decode(fernet_key)
+#    setup(app, EncryptedCookieStorage(secret_key))
+#    setup_routes(app)
+#    app.wslist = []
+#    return app    
 """
 
 routes = \
@@ -79,6 +93,11 @@ async def test_json(request):
     else:
         return web.Response(text='get', headers=headers)
         
+async def broadcast(request, msg):
+    # while True:
+    for ws in request.app.wslist:
+        await ws.send_str(msg)
+    # await asyncio.sleep(10)
         
 async def websocket_handler(request):
 
